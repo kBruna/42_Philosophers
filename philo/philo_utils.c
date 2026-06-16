4efe86db7ae136433c74080d45c684b6fdda10a0
Bruna Uehara <uekabruna@gmail.com>
2026-06-15 23:11:44 -0300

    fix: write lenght read

--- philo/philo_utils.c
+++ philo/philo_utils.c
@@ -21,7 +21,7 @@ int	return_error(char *msg, int flag)
 	{
 		write(STDERR_FILENO, "Recommended Use: >$ ./philosophers \
 <number_of_philosophers> <time_to_die> <time_to_eat> <time_to_sleep> \
-[number_of_times_each_philosopher_must_eat]\n", 150);
+[number_of_times_each_philosopher_must_eat]\n", 149);
 	}
 	return (1);
 }
